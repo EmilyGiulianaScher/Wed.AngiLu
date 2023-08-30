6d615b65bf4677255cc40cd7f873a75bc38236b5
GiulianaScher <emilygiulianascher@gmail.com>
2023-08-29 22:41:27 -0300

    ya casi esta

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const openModal = document.querySelector('.abrir-modal');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal-close');
+
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+
+});
